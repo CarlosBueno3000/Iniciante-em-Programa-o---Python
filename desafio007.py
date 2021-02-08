a90590df7977f2d2a95822a567eabdbc39f95e85
Carlos Alexandre Bueno <76565807+CarlosBueno3000@users.noreply.github.com>
2021-02-08 17:58:08 -0300

    Add files via upload
    O Desafio ou Exercício  7 do Curso em Vídeo de Python.
    
    Um cálculo primário de média de duas notas de um aluno.
    Retorna o resultado com duas casas decimais, de forma simples.

---
+++ desafio007.py
@@ -0,0 +1,5 @@
+#calcular a média de duas notas de um aluno:
+
+n1=float(input('digite a nota 1'))
+n2=float(input('digite a nota 2'))
+print('as notas são {:.2f} e {:.2f}. Sua média final foi {:.2f}'.format(n1,n2,(n1+n2)/2))
